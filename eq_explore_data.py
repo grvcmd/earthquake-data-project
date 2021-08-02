189850e4c36689ed3e528db2c3a5c28460552bd0
Ken <ken.valverde@yahoo.com>
2021-08-02 13:02:33 -0500

    Make a list of all the earthquakes and then extract the magnitudes, longitudes, and latitudes.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,7 +5,17 @@ filename = "data/eq_data_1_day_m1.json"
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = "data/readable_eq_data.json"
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+all_eq_dicts = all_eq_data["features"]
 
+magnitudes, longitudes, latitudes = [], [], []
+for eq_dict in all_eq_dicts:
+    magnitude = eq_dict['properties']['mag']
+    longitude = eq_dict['geometry']['coordinates'][0]
+    latitude = eq_dict['geometry']['coordinates'][1]
+    magnitudes.append(magnitude)
+    longitudes.append(longitude)
+    latitudes.append(latitude)
+
+print(magnitudes[:10])
+print(longitudes[:5])
+print(latitudes[:5])
