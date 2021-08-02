f24f7e73946f68466b14620c43bb6cdf9c3c70b3
Ken <ken.valverde@yahoo.com>
2021-08-02 13:28:24 -0500

    Extract magnitudes, longitudes, and latitudes for world map.

---
+++ eq_world_map.py
@@ -0,0 +1,23 @@
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+filename = "data/readable_eq_data.json"
+with open(filename) as f:
+    all_earthquake_data = json.load(f)
+
+# Create a list of all the earthquakes
+all_earthquake_dicts = all_earthquake_data['features']
+
+# Extract magnitude, longitude, and latitude of all eqs
+magnitudes, longitudes, latitudes = [], [], []
+for earthquake_dict in all_earthquake_dicts:
+    magnitude = earthquake_dict['properties']['mag']
+    longitude = earthquake_dict['geometry']['coordinates'][0]
+    latitude = earthquake_dict['geometry']['coordinates'][1]
+    magnitudes.append(magnitude)
+    longitudes.append(longitude)
+    latitudes.append(latitude)
+
+
