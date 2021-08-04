d4b6b811ae581740c7d54de5c6c9cf40d340906c
Ken V <ken.valverde@yahoo.com>
2021-08-04 17:48:36 -0500

    Add hover text

--- eq_world_map.py
+++ eq_world_map.py
@@ -11,20 +11,23 @@ with open(filename) as f:
 all_earthquake_dicts = all_earthquake_data['features']
 
 # Extract magnitude, longitude, and latitude of all eqs
-magnitudes, longitudes, latitudes = [], [], []
+magnitudes, longitudes, latitudes, hover_texts = [], [], [], []
 for earthquake_dict in all_earthquake_dicts:
     magnitude = earthquake_dict['properties']['mag']
     longitude = earthquake_dict['geometry']['coordinates'][0]
     latitude = earthquake_dict['geometry']['coordinates'][1]
+    title = earthquake_dict['properties']['title']
     magnitudes.append(magnitude)
     longitudes.append(longitude)
     latitudes.append(latitude)
+    hover_texts.append(title)
 
 # Map the earthquakes
 data = [{
     'type': 'scattergeo',
     'lon': longitudes,
     'lat': latitudes,
+    'text': hover_texts,
     'marker': {
         'size': [5*magnitude for magnitude in magnitudes],
         'color': magnitudes,
